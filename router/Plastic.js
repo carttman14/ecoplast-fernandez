1e2633909be08701472b504b1ec594b38a9d036d
carttman14 <carttman14@gmail.com>
2021-07-29 14:59:33 -0500

    plastic view modified

--- router/Plastic.js
+++ router/Plastic.js
@@ -7,7 +7,7 @@ router.get('/',(req,res)=>{
 
     res.render('plastic',{
         plasticArray:[
-            {id:"01", name: 'wicker', description:'Mimbre para múltiples usos'},
+            {id:"01", name: 'mimbre', description:'Mimbre para múltiples usos'},
             {id:"02", name: 'vivo', description:'Vivo para maletines'},
             {id:"03", name: 'enbono', description:'enbono para decoración'},
             {id:"04", name: 'ophelia', description:'Ofelia para decoración'},
